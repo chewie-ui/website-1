47b280952beb84c168386cfd52b025b21720a4b2
Chewie <quentin.rennies@gmail.com>
2026-02-07 09:20:51 +0100

    db logger

--- db/index.js
+++ db/index.js
@@ -1,6 +1,7 @@
 const mongoose = require("mongoose");
 const envName = process.env.NODE_ENV || "development";
 const env = require(`../environment/${envName}`);
+console.log(env.dbUrl);
 
 mongoose
   .connect(env.dbUrl)
